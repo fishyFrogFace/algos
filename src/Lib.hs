243fcbbe5484f46c6d962b96a24dea90d26155cd
Camilla Rygh <camillahallo@mail.com>
2019-09-25 13:14:36 +0200

    Linear search

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib
     ( insertionSort
+    , linearSearch
     ) where
 
 insertionSort :: Ord a => [a] -> [a]
@@ -25,3 +26,12 @@ isort 5 [1,2,3]
 1:2:(isort 5 [3])
 1:2:3:(isort 5 [])
 [1,2,3,5] -}
+
+linearSearch :: Eq a => [a] -> a -> Maybe Int
+linearSearch lst val = lin lst val 0
+    where
+        lin :: Eq a => [a] -> a -> Int -> Maybe Int
+        lin [] _ _ = Nothing
+        lin (x:xs) val i
+            | x == val  = Just i
+            | otherwise = lin xs val (i+1)
