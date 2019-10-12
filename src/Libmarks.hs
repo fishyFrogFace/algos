85045512fcf0abcd81eb1a3caf30bd706f71a3d6
Camilla Rygh <camillahallo@mail.com>
2019-10-12 11:48:24 +0200

    Compare Data.List.sort with Lib.mergeSort

---
+++ src/Libmarks.hs
@@ -0,0 +1,11 @@
+module Libmarks
+  ( sortVsMergeSort
+  ) where
+
+import Data.List (sort)
+
+import Benchmark
+import Lib
+
+sortVsMergeSort :: IO ()
+sortVsMergeSort = vectorsOfIntBench "sort" "mergeSort" sort mergeSort 
